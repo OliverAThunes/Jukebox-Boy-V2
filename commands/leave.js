4903bb70d6ff84f9afc05d79f6bcaecdb5551c81
Oliver Andreas Thunæs <oliver@netron.no>
2021-10-13 09:30:22 +0200

    Leave command (untested)

--- commands/leave.js
+++ commands/leave.js
@@ -1,13 +1,18 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { VoiceChannel } = require('discord.js');
-const { joinVoiceChannel, entersState, VoiceChannelConnectionStatus } = require('@discordjs/voice')
-const ytdl = require('ytdl-core');
-const ytSearch = require('yt-search');
+const { getVoiceConnection } = require('@discordjs/voice')
 
 module.exports = {
   data: new SlashCommandBuilder()
   .setName('leave')
-  .setDescription('Joins voice channel'),
+  .setDescription('Leaves voice channel'),
   async execute(interaction, client) {
+    const connection = getVoiceConnection(process.env.GUILD_ID);
+
+    if (!connection) {
+      await interaction.reply('Not in a voice channel!');
+    } else {
+      connection.destroy();
+    }
+
   }
 }
